3a5ab5611390818bfc5b489d2b445f0a4015194c
speedy27 <comptezoom1@.com>
2025-04-11 14:47:03 -0400

    apresBUILD

---
+++ build/CMakeFiles/PokemonCpp.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kali/Documents/C++/GameC--1/src/Battle.cpp" "CMakeFiles/PokemonCpp.dir/src/Battle.cpp.o" "gcc" "CMakeFiles/PokemonCpp.dir/src/Battle.cpp.o.d"
+  "/home/kali/Documents/C++/GameC--1/src/Game.cpp" "CMakeFiles/PokemonCpp.dir/src/Game.cpp.o" "gcc" "CMakeFiles/PokemonCpp.dir/src/Game.cpp.o.d"
+  "/home/kali/Documents/C++/GameC--1/src/Pokemon.cpp" "CMakeFiles/PokemonCpp.dir/src/Pokemon.cpp.o" "gcc" "CMakeFiles/PokemonCpp.dir/src/Pokemon.cpp.o.d"
+  "/home/kali/Documents/C++/GameC--1/src/SFMLGame.cpp" "CMakeFiles/PokemonCpp.dir/src/SFMLGame.cpp.o" "gcc" "CMakeFiles/PokemonCpp.dir/src/SFMLGame.cpp.o.d"
+  "/home/kali/Documents/C++/GameC--1/src/main.cpp" "CMakeFiles/PokemonCpp.dir/src/main.cpp.o" "gcc" "CMakeFiles/PokemonCpp.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/PokemonCpp.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PokemonCpp.dir/src/Battle.cpp.o"
+  "CMakeFiles/PokemonCpp.dir/src/Battle.cpp.o.d"
+  "CMakeFiles/PokemonCpp.dir/src/Game.cpp.o"
+  "CMakeFiles/PokemonCpp.dir/src/Game.cpp.o.d"
+  "CMakeFiles/PokemonCpp.dir/src/Pokemon.cpp.o"
+  "CMakeFiles/PokemonCpp.dir/src/Pokemon.cpp.o.d"
+  "CMakeFiles/PokemonCpp.dir/src/SFMLGame.cpp.o"
+  "CMakeFiles/PokemonCpp.dir/src/SFMLGame.cpp.o.d"
+  "CMakeFiles/PokemonCpp.dir/src/main.cpp.o"
+  "CMakeFiles/PokemonCpp.dir/src/main.cpp.o.d"
+  "PokemonCpp"
+  "PokemonCpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PokemonCpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
